688f31b51581321af5e7539a622ec7018999e48a
jowc <jchikeme@gmail.com>
2022-03-18 16:35:13 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { LottieModule } from 'ngx-lottie';
+import player from 'lottie-web';
+
 import { AppComponent } from './app.component';
+
+
+export function playerFactory() {
+  return player;
+}
+
 
 @NgModule({
   declarations: [
@@ -10,7 +19,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LottieModule.forRoot({ player: playerFactory }),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	mode: 'jit',
+      content: [
+    // Example content paths...
+    './dist/**/*.html',
+    './src/**/*.{js,ts, html}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
