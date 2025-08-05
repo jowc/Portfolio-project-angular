677a659356248a49558708fb67f167283d94f58c
Joseph Chikeme <jchikeme@gmail.com>
2025-08-05 16:35:14 +0300

    fixed services unit test error

--- src/app/shared/components/projects-card/projects-service.service.spec.ts
+++ src/app/shared/components/projects-card/projects-service.service.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ProjectsService } from './projects-service.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('ProjectsService', () => {
   let service: ProjectsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [ProjectsService],
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(ProjectsService);
   });
 
