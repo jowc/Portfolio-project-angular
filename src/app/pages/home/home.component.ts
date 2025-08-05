8c32d08bd1a77878b493d8b0e35925b495387db4
Joseph Chikeme <jchikeme@gmail.com>
2025-08-05 10:46:39 +0300

    installed deps

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,26 +7,17 @@ import {
   ViewChild,
 } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-// import { ActivatedRoute } from '@angular/router';
-// import { select, Store } from '@ngrx/store';
 
 import { AnimationOptions } from 'ngx-lottie';
-import { map, Observable, of, take, tap } from 'rxjs';
-// import { ProjectsService } from 'src/app/shared/components/projects-card/projects-service.service';
+import { Observable, of } from 'rxjs';
 import { projects } from 'src/app/store/api';
-// import * as fromRoot from 'src/app/store/app.state';
 import { projectModel } from 'src/app/store/models';
-import { Container, Main } from 'tsparticles';
-// import { retrieveProject } from '../store/pages.action';
-// import { selectProjectCollection } from '../store/pages.selectors';
 import { particleOptionsJSON } from './particlesjs-config';
-// import { fade } from 'src/app/shared/animation';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  // animations: [fade]
 })
 export class HomeComponent implements AfterViewInit, OnInit, OnDestroy {
   id = 'z';
@@ -38,7 +29,7 @@ export class HomeComponent implements AfterViewInit, OnInit, OnDestroy {
 
   projects$!: Observable<any>;
   projects!: projectModel[];
-  projectData$: projectModel[] = [];
+  projectData: projectModel[] = [];
   projectEnd: boolean = false;
 
   /* or the classic JavaScript object */
@@ -59,22 +50,13 @@ export class HomeComponent implements AfterViewInit, OnInit, OnDestroy {
   constructor(
     // private route: ActivatedRoute,
     // private store: Store<fromRoot.AppState>,
-    private title: Title
-  ) // private projectAPI: ProjectsService
-  {}
+    private title: Title // private projectAPI: ProjectsService
+  ) {}
 
   ngOnInit(): void {
     this.title.setTitle('Joseph O. Chikeme');
-    // this.projects$ = this.store.select("projects").pipe(map((data: any) => data.projects))
-    // this.route.fragment.subscribe((e: any) => {
-    //   if (e) {
-    //     this.scroll(e)
-    //   }
-    // })
 
     this.getProjects();
-    // this.store.select("projects").subscribe(data => console.log(data))
-    // this.store.dispatch(retrieveProject())
   }
 
   ngOnDestroy(): void {
@@ -86,43 +68,24 @@ export class HomeComponent implements AfterViewInit, OnInit, OnDestroy {
     // console.log(this.div)
   }
 
-  particlesLoaded(container: Container): void {
-    // console.log(container.canvas.size);
-  }
-
-  particlesInit(main: Main): any {
-    // main.set('z', this.div)
-    // console.log(main);
-    return;
-  }
-
   scroll(el: any) {
     el.scrollIntoView();
   }
 
   getProjects() {
-    // this.projects = this.store.select("projects")
-    // this.projectAPI.getProjects().subscribe(data => console.log(data.data))
-    // return this.store.pipe(select(selectProjectCollection)).subscribe((data: any) => {
-    //   this.projects = data.projects.slice(0, 3)
-    //   this.projectData$ = data.projects
-    // })
-
     return of(projects)
       .subscribe((data) => {
         // console.log('projects local: ', data)
         this.projects = data;
-        this.projectData$ = data.slice(0, 3);
+        this.projectData = data.slice(0, 3);
       })
       .unsubscribe();
   }
 
   getProject() {
-    let projectCount = +this.projects.length;
-    this.projectData$ = this.projects.slice(0, this.projectData$.length + 3);
-    // console.log("worked")
+    this.projectData = this.projects.slice(0, this.projectData.length + 3);
 
-    if (this.projects.length == this.projectData$.length) {
+    if (this.projects.length == this.projectData.length) {
       this.projectEnd = true;
     }
   }
