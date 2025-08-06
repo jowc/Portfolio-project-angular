ec379b13236ca59141aea013402e443634e5fd8c
Joseph Chikeme <jchikeme@gmail.com>
2025-08-06 14:17:03 +0300

    added header test

--- src/app/shared/components/header/header.component.spec.ts
+++ src/app/shared/components/header/header.component.spec.ts
@@ -3,10 +3,13 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HeaderComponent } from './header.component';
 import { SharedModule } from '../../shared.module';
 import { provideRouter } from '@angular/router';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let el: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -19,9 +22,23 @@ describe('HeaderComponent', () => {
 
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
+    el = fixture.debugElement;
   });
 
   it('should create header component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('show header menu when clicked', () => {
+    const toggleBtn = el.query(By.css('[data-test-id="menu-toggle"]'));
+    let dropMenu = el.query(By.css('[data-test-id="header-drop-menu"]'));
+    expect(component.isOpen).toBe(false);
+    expect(dropMenu?.nativeElement).toBeFalsy();
+
+    (toggleBtn.nativeElement as HTMLDivElement)?.click();
+    fixture.detectChanges();
+    dropMenu = el.query(By.css('[data-test-id="header-drop-menu"]'));
+    expect(component.isOpen).toBe(true);
+    expect(dropMenu?.nativeElement).toBeTruthy();
   });
 });
