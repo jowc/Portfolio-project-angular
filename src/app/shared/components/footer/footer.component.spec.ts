ca8d90e3b9807b549f32728d1b17bc8b2d03db7f
Joseph Chikeme <jchikeme@gmail.com>
2025-08-06 14:35:57 +0300

    added footer test

--- src/app/shared/components/footer/footer.component.spec.ts
+++ src/app/shared/components/footer/footer.component.spec.ts
@@ -1,25 +1,35 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FooterComponent } from './footer.component';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('FooterComponent', () => {
   let component: FooterComponent;
   let fixture: ComponentFixture<FooterComponent>;
+  let el: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FooterComponent ]
-    })
-    .compileComponents();
-  });
+      declarations: [FooterComponent],
+    }).compileComponents();
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(FooterComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    el = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should contain Github, linkedIn, and Twitter links', () => {
+    const socialLinks = el.queryAll(By.css('.sMedia a'));
+
+    const links = socialLinks.map((item) => item.nativeElement?.href).join(',');
+
+    expect(links).toBe(
+      'https://github.com/jowc,https://www.linkedin.com/in/ojiofor-chikeme/,https://twitter.com/ojiofor_j'
+    );
   });
 });
