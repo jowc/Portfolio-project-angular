417924c3a97f2a2c3d141cce5d55843235547311
Joseph Chikeme <jchikeme@gmail.com>
2025-08-06 13:10:09 +0300

    Added app component test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
   let fixture: ComponentFixture<AppComponent>;
   let app: AppComponent;
+  let el: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -13,9 +16,25 @@ describe('AppComponent', () => {
 
     fixture = TestBed.createComponent(AppComponent);
     app = fixture.componentInstance;
+    el = fixture.debugElement;
   });
 
   it('should create the app', () => {
     expect(app).toBeTruthy();
+  });
+
+  it('should have header component', () => {
+    const headerComponent = el.query(By.css('app-header'));
+    expect(headerComponent).toBeTruthy();
+  });
+
+  it('should have footer component', () => {
+    const headerComponent = el.query(By.css('app-footer'));
+    expect(headerComponent).toBeTruthy();
+  });
+
+  it('should have router outlet', () => {
+    const headerComponent = el.query(By.css('router-outlet'));
+    expect(headerComponent).toBeTruthy();
   });
 });
