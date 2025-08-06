ba8d2ea825a4ce7c78ca159f121552d84c862474
Joseph Chikeme <jchikeme@gmail.com>
2025-08-06 15:25:38 +0300

    added home component unit test

--- src/app/pages/home/home.component.spec.ts
+++ src/app/pages/home/home.component.spec.ts
@@ -1,25 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let el: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
-    })
-    .compileComponents();
-  });
+      declarations: [HomeComponent],
+    }).compileComponents();
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    el = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have particles js component', () => {
+    const component = el.query(By.css('ngx-particles'));
+    expect(component).toBeTruthy();
+  });
+
+  it('should have lottie component', () => {
+    const component = el.query(By.css('ng-lottie'));
+    expect(component).toBeTruthy();
+  });
 });
+
