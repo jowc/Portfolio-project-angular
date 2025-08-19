0a8a2863eb4ef25e1e0183b44448e54723e1ef44
Joseph Chikeme <jchikeme@gmail.com>
2025-08-19 21:26:00 +0300

    refactor: standardize string quotes in Cypress tests and improve contact form validation

--- cypress/e2e/home/home.cy.ts
+++ cypress/e2e/home/home.cy.ts
@@ -1,8 +1,25 @@
 describe("Home Page", () => {
+
+    beforeEach(() => {
+        cy.visit("localhost:4200")
+    })
     
 
     it("should have Joseph Chikeme", () => {
-        cy.visit("localhost:4200")
         cy.get("h1").contains("Joseph Ojiofor Chikeme")
     })
-})
+
+    it("should load projects", () => {
+        cy.get("app-projects-card").should("have.length.above", 2)
+    })
+
+    it("hero let's chat button should redirect to contact page", () => {
+        cy.contains("Let's Chat").first().click()
+        cy.url().should("equal", "http://localhost:4200/contact")
+    })
+
+    it("CTA let's chat button should redirect to contact page", () => {
+        cy.contains("Let's Chat").last().click()
+        cy.url().should("equal", "http://localhost:4200/contact")
+    })
+}) 
